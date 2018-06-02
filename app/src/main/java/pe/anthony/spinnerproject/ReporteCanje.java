aeec1f014b8eea4705df5f343718c0451a10451a
anthony puitiza <anthony.puitiza@unmsm.edu.pe>
2018-06-02 15:14:27 -0500

    Limpiando imports

--- app/src/main/java/pe/anthony/spinnerproject/ReporteCanje.java
+++ app/src/main/java/pe/anthony/spinnerproject/ReporteCanje.java
@@ -1,14 +1,11 @@
 package pe.anthony.spinnerproject;
 
 import android.content.Intent;
-import android.content.SharedPreferences;
-import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextUtils;
 import android.text.TextWatcher;
-import android.util.Log;
 import android.view.View;
 import android.view.Window;
 import android.widget.ArrayAdapter;
